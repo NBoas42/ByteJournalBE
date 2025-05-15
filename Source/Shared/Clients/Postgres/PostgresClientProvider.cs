ea5a0648b41d1a87631795f7d8abc498a01b70a6
Nathaniel Boas <nbboas@gmail.com>
2025-05-14 19:34:08 -0700

    Added Styling and Moved local config file

--- Source/Shared/Clients/Postgres/PostgresClientProvider.cs
+++ Source/Shared/Clients/Postgres/PostgresClientProvider.cs
@@ -1,22 +1,23 @@
 using Npgsql;
 public class PostgresClientProvider {
-    private  NpgsqlConnection postgresClient;
-    public PostgresClientProvider(AppConfig config){
+    private NpgsqlConnection postgresClient;
+    public PostgresClientProvider(AppConfig config) {
+
         string databaseURL = config.Get("databaseURL");
         string port = config.Get("port");
         string databaseName = config.Get("databaseName");
         string userId = config.Get("userId");
-        string password =config.Get("password");
+        string password = config.Get("password");
         string connString = $"Host={databaseURL};Port={port};Database={databaseName};Username={userId};Password={password};";
 
-
         Console.WriteLine($"Connecting to Postgres at {connString}");
+        
         this.postgresClient = new NpgsqlConnection(connString);
     }
 
-    public NpgsqlConnection getPostgresClient () {
+    public NpgsqlConnection getPostgresClient() {
         return this.postgresClient;
     }
-    
+
 
 }
