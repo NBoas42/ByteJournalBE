c07f7b48370412daf8cad857401a2e3c4fae1523
Nathaniel Boas <nbboas@gmail.com>
2025-05-14 19:51:59 -0700

    Refresh Git index with updated .gitignore

--- Source/Domains/Account/Controller/AccountController.cs
+++ Source/Domains/Account/Controller/AccountController.cs
@@ -3,16 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 [ApiController]
 [Route("api/account")]
 public class AccountController : ControllerBase {
+
     private readonly AccountResource accountResource;
+
     public AccountController(AccountResource accountResource) {
         this.accountResource = accountResource;
     }
 
     [HttpGet("search")]
     public async Task<IActionResult> SearchAccounts() {
-
         Account[] results = await this.accountResource.searchAccounts();
 
-        return Ok(results);
+        return Ok(results); 
     }
+
 }
